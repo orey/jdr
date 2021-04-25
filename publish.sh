b3e93a2fe7fe57f36a1c4b33160cccc9b15e8de1
orey <rey.olivier@gmail.com>
2021-04-25 08:24:07 +0200

    new test

--- publish.sh
+++ publish.sh
@@ -1,4 +1,19 @@
-#/bin/bash
+#!/bin/sh
+clear
+
+if [ $# != 1 ]
+then
+    echo "Git publish script version 1"
+    echo "Usage : publish.sh [comment_between_quotes]"
+    exit 0
+fi
+
 git add *
 git commit -a -m "$1"
 git push origin master
+
+echo "Done"
+
+
+
+

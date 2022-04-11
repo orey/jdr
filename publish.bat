42a52e0cd0414f3e8d2578175672e91662968cc7
Olivier Rey <rey.olivier@gmail.com>
2022-04-11 21:01:35 +0200

    Introduction du repo Risus

---
+++ publish.bat
@@ -0,0 +1,16 @@
+cls
+@echo off
+
+if "%1" == "" ( 
+    echo Git publish script version 1
+    echo Usage : publish.sh [comment_between_quotes]
+    goto :end
+)
+
+git add *
+git commit -a -m "%*"
+git push origin mastar
+
+:end
+echo Done
+

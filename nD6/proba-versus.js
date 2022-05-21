23e4fc751ea2eea274d712f00453815c9ea61a81
orey <rey.olivier@gmail.com>
2022-05-21 16:10:03 +0200

    Nouveau tool pour déterminer les probas de n D

--- nD6/proba-versus.js
+++ nD6/proba-versus.js
@@ -34,6 +34,10 @@ if (isNaN(number1) || isNaN(faces1) || isNaN(number2) || isNaN(faces2)){
 myconsole.log(  number1.toString() + " - " + faces1.toString() + " - "
               + number2.toString() + " - " + faces2.toString() );
 
+function myPercentFormat(i){
+    return (i*100).toFixed(3).toString() + "%";
+}
+
 
 class Fighter {
     constructor(number, faces){
@@ -75,35 +79,64 @@ class Fighter {
     printCombiNb(){
         let siz = this.combinb.length;
         let val = 0
+        let maxcombi = Math.pow(this.faces,this.number);
+        console.log("--------------------------------------------");
+        console.log("Value|Nb of combinations|%");
         for (let i=0;i<siz;i++){
             val = this.combinb[i];
             if (val != undefined)
-                console.log(i + " : " + val);
+                console.log(i + "|" + val + "|" + myPercentFormat(val/maxcombi));
         }
     }
 
-    moreThanOrEqualToTargetNumber(tn){
+    moreOrEqualThanTargetNumber(tn){
         if (tn > this.number*this.faces)
             return 0;
+        if (tn <= this.number)
+            return 1; // 100%
         let siz = this.combinb.length;
         let sub = this.combinb.slice(tn,siz);
+        if (sub.length == 0) return NaN;
         return sub.reduce(function(a, b){return a+b;}) / Math.pow(this.faces,this.number);    
     }
 
-    percentageMoreThanOrEqualToTargetNumber(format=1){
+    strictlyMoreThanTargetNumber(tn){
+        if (tn >= this.number*this.faces)
+            return 0;
+        let siz = this.combinb.length;
+        let sub = this.combinb.slice(tn+1,siz);
+        if (sub.length == 0) return NaN;
+        return sub.reduce(function(a, b){return a+b;}) / Math.pow(this.faces,this.number);    
+    }
+
+    strictlyLessThanTargetNumber(tn){
+        if (tn <= this.number)
+            return 0;
+        let siz = this.combinb.length;
+        let sub = this.combinb.slice(this.number,tn);
+        if (sub.length == 0) return NaN;
+        return sub.reduce(function(a, b){return a+b;}) / Math.pow(this.faces,this.number);    
+    }
+
+    lessOrEqualThanTargetNumber(tn){
+        if (tn > this.number*this.faces)
+            return 0;
+        let siz = this.combinb.length;
+        let sub = this.combinb.slice(this.number,tn+1);
+        if (sub.length == 0) return NaN;
+        return sub.reduce(function(a, b){return a+b;}) / Math.pow(this.faces,this.number);    
+    }
+    
+    percentagesComparedToTargetNumber(){
         let max = this.number*this.faces;
+        console.log("--------------------------------------------");
+        console.log("TN|More of equal|Strictly more|Less or equal|Strictly less");
         for (let tn=this.number;tn<=max;tn++){
-            if (format !=1)
-                console.log("TN = "
-                            + tn.toString()
-                            + " - "
-                            + (this.moreThanOrEqualToTargetNumber(tn)*100).toFixed(2).toString()
-                            + "%");
-            else
-                console.log(tn.toString()
-                            + ","
-                            + (this.moreThanOrEqualToTargetNumber(tn)*100).toFixed(2).toString()
-                            + "%");
+            console.log(tn.toString() + " | "
+                        + myPercentFormat(this.moreOrEqualThanTargetNumber(tn)) + " | "
+                        + myPercentFormat(this.strictlyMoreThanTargetNumber(tn)) + " | "
+                        + myPercentFormat(this.lessOrEqualThanTargetNumber(tn)) + " | "
+                        + myPercentFormat(this.strictlyLessThanTargetNumber(tn)) );
         }
     }
 
@@ -116,17 +149,47 @@ class Fighter {
         myconsole.log(this.check());
         //myconsole.log(this.combinb);
         console.log("=> " + Math.pow(this.faces,this.number).toString() + " combinations");
-        this.printCombiNb();
-        this.percentageMoreThanOrEqualToTargetNumber();
+        //this.printCombiNb();
+        this.percentagesComparedToTargetNumber();
     }
 
 }
 
+// we are on the scale of fighter1
+// we start at TN = number1
+// we stop  at TN = number1 * faces1
 let fighter1 = new Fighter(number1,faces1);
 fighter1.calculate();
 
 let fighter2 = new Fighter(number2,faces2);
 fighter2.calculate();
+
+// seen from fighter1
+let win=0, loose=0;
+console.log("--------------------------------------------");
+console.log("--- Seen from fighter 1 ---");
+
+let max = number1 * faces1;
+for (let tn=number1;tn<=max;tn++){
+    // real win
+    let a = fighter1.moreOrEqualThanTargetNumber(tn);
+    let b = fighter2.strictlyLessThanTargetNumber(tn);
+    console.log("WIN");
+    console.log(a);
+    console.log(b);
+    let temp1 = a*b;
+    win += temp1;
+    //real loose
+    a = fighter1.strictlyLessThanTargetNumber(tn);
+    b = fighter2.moreOrEqualThanTargetNumber(tn);
+    console.log("LOOSE");
+    console.log(a);
+    console.log(b);
+    let temp2 =  a*b;
+    myconsole.log("TN = " + tn.toString() + " - WIN % : " +  myPercentFormat(temp1) + " - LOOSE % : " + myPercentFormat(temp2));
+    loose += temp2;
+}
+
 
 
 
